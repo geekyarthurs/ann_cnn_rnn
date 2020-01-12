282ffa38b7f2432366d5fff166dadab51ed2b803
Mahesh C. Regmi <geekyarthurs@gmail.com>
2020-01-12 16:30:51 +0545

    GRID FITTING ADDED

--- ann.py
+++ ann.py
@@ -1,17 +1,17 @@
 # Artificial Neural Network
 
 
-# Importing the libraries
+# %% Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# %% Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
-
+# %% PREPROCESSING DATA
 
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
@@ -43,20 +43,21 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Now let's make the ANN!
+# %% Part 2 - Now let's make the ANN!
 
 # Importing the Keras libraries and packages
 import keras
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 
 
 classifier = Sequential()
 #1st layer
 classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_shape=(11,)))
+classifier.add(Dropout(rate=0.1))
 #2nd layer
 classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
-
+classifier.add(Dropout(rate=0.1))
 #Final layer
 classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
 
@@ -73,13 +74,42 @@ from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
 
+# Part 4 - Evaluating, Improving and Tuning the ANN
 
+# %% Evaluating the ANN
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu',input_shape=(11,)))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
 
+#%%
+#classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100)
+#accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = -1)
+#mean = accuracies.mean()
+#variance = accuracies.std()
 
+# %% 
 
+classifier = KerasClassifier(build_fn = build_classifier)
 
+parameters = {
+    
+    'batch_size' : [25, 32],
+    'epochs' : [100 , 500],
+    'optimizer' : ['adam', 'rmsprop']
 
-
+    }
+gsv = GridSearchCV(estimator = classifier, param_grid = parameters, scoring='accuracy', cv = 10)
+gsv = gsv.fit(X_train, y_train)
+best_parameters = gsv.best_params_
+best_accuracy = gsv.best_score_
 
 
 
